df73d850db1d92d899e19e0f48457905ce90f3f0
imkimheejoo <jujudamdadi@naver.com>
2023-01-26 00:55:40 +0900

    project init

---
+++ src/main/kotlin/com/study/newtil/NewTilApplication.kt
@@ -0,0 +1,11 @@
+package com.study.newtil
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NewTilApplication
+
+fun main(args: Array<String>) {
+	runApplication<NewTilApplication>(*args)
+}
---
+++ src/test/kotlin/com/study/newtil/NewTilApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.newtil
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NewTilApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
